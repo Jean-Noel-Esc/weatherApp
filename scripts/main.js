082a6618fc8d70526e8c1228f73eef5321c3c150
Jean-Noel-Esc <60701865+Jean-Noel-Esc@users.noreply.github.com>
2021-07-15 17:51:11 +0200

    premier appel API

--- scripts/main.js
+++ scripts/main.js
@@ -4,8 +4,22 @@ let resultatsApi;
 
 if(navigator.geolocation){
     navigator.geolocation.getCurrentPosition(position => {
-        console.log(position);
+        //console.log(position);
+        let long = position.coords.longitude;
+        let lat = position.coords.latitude;
+        appelApi(long,lat)
     }, ()=> {
         alert('Vous avez refusé la geolocalisation')
+    })
+}
+
+function appelApi(long,lat){
+    //console.log(long,lat);
+    fetch(`https://api.openweathermap.org/data/2.5/onecall?lat=${lat}&lon=${long}&exclude=minutely&units=metric&lang=fr&appid=${keyApi}`)
+    .then((reponse) => {
+        return reponse.json();
+    })
+    .then((data) => {
+        console.log(data);
     })
 }
